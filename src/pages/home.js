f3cc57dffbf6821965bd3b6dc7ebd64d9210e61a
kisabellecortez <kisabellecortez@gmail.com>
2024-06-07 18:35:28 -0400

    developed styling for programs - lessons

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,19 @@ const Home = () => {
     return(
         <div className='home'>
             <TopNav/>
-            <h1>Welcome to the Scarborough Recreation Center</h1>
 
-            <h2>Who we are</h2>
-            <p>The Scarborough Recreation Center located in the heart of Scarborough, is a non-profit center founded in 1982. All proceeds go toward funding our youth programs and providing accessible state of the art equipment for the community. We have a wide variety of facilities: an olympic sized pool, 4 tennis courts, 2 gymnasiums, a commerical gym, a soccer field, two studio rooms, and an array of equipment for a variety of sports. We are dedicated to bettering the community by providing accessible opportunities for the youth. </p>
+            <div>
+                <div className='about'>
+                    <h1>Welcome to Scarbs Rec!</h1>
+
+                    <h2>ABOUT US</h2>
+                    <p>Welcome to the Scarborough Community Center, where our mission is to provide accessible and enriching opportunities for youth in our vibrant community. Located in the heart of Scarborough, our center is dedicated to empowering young individuals by offering a wide range of programs and activities designed to inspire, educate, and engage. From sports and fitness classes to arts, culture, and educational workshops, we strive to create a nurturing environment where youth can explore their interests, develop new skills, and build lasting friendships. Our committed team of professionals and volunteers work tirelessly to ensure that every young person has the chance to thrive, regardless of their background or circumstances. Join us at the Scarborough Community Center and be part of a community that values growth, inclusivity, and the limitless potential of our youth. Together, we can make a difference and help shape a brighter future for all.</p>
+                </div>
+
+                <div className='rec-img'>
+                    <img src="https://www.hbaonline.com/wp-content/uploads/2014/01/HBA_Kempsville_Community_Recreation_Center01.jpg" alt="Scarbs Rec"></img>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/programsLessons.js
+++ src/pages/programsLessons.js
@@ -1,16 +1,22 @@
+import React, { useState } from 'react';
+
 import TopNav from '../components/TopNav.js'
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
 import { lessonsList } from '../data/lessonsList.js'
 
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function srcset(image, size, rows = 1, cols = 1) {
     return {
@@ -22,37 +28,133 @@ function srcset(image, size, rows = 1, cols = 1) {
   }
 
 const ProgramsLessons = () => {
+  const [type, setType] = useState('')
+  const [ages, setAges] = useState('')
+  const [level, setLevel] = useState('')
+
+  const handleChangeType = (event) => {
+    setType(event.target.value)
+  }
+
+  const handleChangeAges = (event) => {
+    setAges(event.target.value)
+  }
+
+  const handleChangeLevel = (event) => {
+    setLevel(event.target.value)
+  }
+
     return(
         <div className="programs-lessons">
             <TopNav/>
 
-            <div className='description'>
-                <h1>LESSONS</h1>
-                <p>We offer a wide range of instructional lessons for each age group. Classes are two months long with one 1 hour lesson a week, most instructional sports are offered year round with the exception of tennis and soccer which are only offered in the summer. Sign up for classes will open a month before, there is limited space so be sure to be the first to register!</p> 
+            <div className="description">
+              <div>
+                  <h1>LESSONS</h1>
+                  <p>Discover Your Passion: Scarbs Rec offers an exciting array of enriching lessons tailored to ignite creativity and cultivate athleticism. Dive into our vibrant sports program, where seasoned coaches guide participants of all ages through dynamic training sessions, fostering teamwork and skill development in a variety of sports disciplines. For those inclined towards the arts, explore our diverse range of artistic endeavors, from painting and pottery to music and dance. Unleash your potential in a welcoming and supportive environment, where learning knows no bounds. Join us and embark on a journey of self-discovery and growth!</p> 
+              </div>
+
+              <div>
+                <div className='seasonal-card'>
+                  <h3>YEAR ROUND</h3>
+                  <p>BADMINTON</p>
+                  <p>BALLET</p>
+                  <p>BASKETBALL</p>
+                  <p>GUITAR</p>
+                  <p>SWIMMING</p>
+                  <p>VOLLEYBALL</p>
+                </div>
+
+                <div className='seasonal-card'>
+                  <h3>SUMMER (MAY TO AUGUST)</h3>
+                  <p>SOCCER</p>
+                  <p>TENNIS</p>
+                </div>
+              </div>
             </div>
 
             <div className='photos'>
-                <ImageList
-                    sx={{ width: 500, height: 500 }}
-                    variant="quilted"
-                    cols={4}
-                    rowHeight={121}
-                    >
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                        <img
-                            {...srcset(item.img, 121, item.rows, item.cols)}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                        </ImageListItem>
-                    ))}
-                </ImageList>
+                  <ImageList
+                      sx={{ width: 500, height: 500 }}
+                      variant="quilted"
+                      cols={4}
+                      rowHeight={121}
+                      >
+                      {itemData.map((item) => (
+                          <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+                          <img
+                              {...srcset(item.img, 121, item.rows, item.cols)}
+                              alt={item.title}
+                              loading="lazy"
+                          />
+                          </ImageListItem>
+                      ))}
+                  </ImageList>
+            </div>
+
+            <div className='filters'>
+              <h3>FILTERS</h3>
+
+              <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">TYPE</InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={type}
+                  label="TYPE"
+                  onChange={handleChangeType}
+                >
+                  <MenuItem value="">
+                    <em>ALL</em>
+                  </MenuItem>
+                  <MenuItem value={"BASKETBALL"}>BASKETBALL</MenuItem>
+                  <MenuItem value={"SOCCER"}>SOCCER</MenuItem>
+                  <MenuItem value={"VOLLEYBALL"}>VOLLEYBALL</MenuItem>
+                </Select>
+              </FormControl>
+
+              <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">AGE GROUP</InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={ages}
+                  label="AGE GROUP"
+                  onChange={handleChangeAges}
+                >
+                  <MenuItem value="">
+                    <em>ALL</em>
+                  </MenuItem>
+                  <MenuItem value={"8 TO 10"}>8 TO 10</MenuItem>
+                  <MenuItem value={"11 TO 14"}>11 TO 14</MenuItem>
+                  <MenuItem value={"15 TO 17"}>15 TO 17</MenuItem>
+                  <MenuItem value={"18 TO 22"}>18 TO 22</MenuItem>
+                  <MenuItem value={"22+"}>22+</MenuItem>
+                </Select>
+              </FormControl>
+
+              <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">LEVEL</InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={level}
+                  label="LEVEL"
+                  onChange={handleChangeLevel}
+                >
+                  <MenuItem value="">
+                    <em>ALL</em>
+                  </MenuItem>
+                  <MenuItem value={"BEGINNER"}>BEGINNER</MenuItem>
+                  <MenuItem value={"INTERMEDIATE"}>INTERMEDIATE</MenuItem>
+                  <MenuItem value={"ADVANCED"}>ADVANCED</MenuItem>
+                </Select>
+              </FormControl>
             </div>
 
             <div className="lesson-card-section">
                 {lessonsList.map((lesson) => (
-                    <div key={lesson.id} className='lessons-card'>
+                    <div key={lesson.id} className='lesson-card'>
                         <Card sx={{ minWidth: 800, maxWidth: 800, minHeight: 400, maxHeight: 400 }}>
                             <CardContent>
                                 <h2>{lesson.title}</h2>
@@ -75,6 +177,8 @@ const ProgramsLessons = () => {
                                 <Button variant="contained" color="success">
                                 REGISTER
                                 </Button>
+
+                                <Button >SCHEDULE</Button>
                             </CardActions>
                         </Card>
                     </div>
