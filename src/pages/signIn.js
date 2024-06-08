0b56777d551b6e8e5d1db7b540846566ef6e6010
kisabellecortez <kisabellecortez@gmail.com>
2024-06-07 21:57:04 -0400

    styling for sign in and sign up pages

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import Alert from '@mui/material/Alert';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import { UserAuth } from '../context/AuthContext.js'
 import { useNavigate } from 'react-router-dom'
 
@@ -8,8 +13,21 @@ const SignIn = () => {
     const { signIn } = UserAuth()
     const navigate = useNavigate(); 
 
+    const [open, setOpen] = useState(false);
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const handleError = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
 
     const handleSignIn = async(e) => {
         try{
@@ -17,41 +35,58 @@ const SignIn = () => {
             navigate('/home')
         }
         catch(userCredential){
-            alert('Invalid email or password.')
+            handleError(); 
         }
     }
 
     return(
         <div className="sign-in">
-            <div className="card">
-                <h1>Sign In</h1>
+           
+            <Card sx={{ minWidth: 300, minHeight: 375 }}>
+                <CardContent>
+                    <h1>Sign In</h1>
+                    <div className='form-field'>
+                        <TextField
+                            required
+                            value={email}
+                            id="email"
+                            label="Email"
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
 
-                <div>
-                    <TextField
-                        required
-                        value={email}
-                        id="email"
-                        label="Email"
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
+                    <div className='form-field'>
+                        <TextField
+                            required
+                            value={password}
+                            id="password"
+                            label="Password"
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
 
-                <div>
-                    <TextField
-                        required
-                        value={password}
-                        id="password"
-                        label="Password"
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
+                    <p>Don't have an account? Sign up <a href='/sign-up'>here</a>!</p>
+                </CardContent>
+                <CardActions>
+                    <Button onClick={handleSignIn} variant="contained">Sign In</Button>
 
-                <Button onClick={handleSignIn} variant="contained">Sign In</Button>
+                </CardActions>
+            </Card>
 
-                <p>Don't have an account? Sign up <a href='/sign-up'>here</a>!</p>
-            </div>
+            <Snackbar
+                open={open}
+                autoHideDuration={5000}
+                onClose={handleClose}
+            >
+                <Alert 
+                    onClose={handleClose}
+                    severity="error"
+                >
+                    Invalid email or password.
+                </Alert>
+            </Snackbar> 
         </div>
     )
 }
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
 import { UserAuth } from '../context/AuthContext.js'
 import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
     const { createUser } = UserAuth(); 
     const navigate = useNavigate(); 
+
+    const [open, setOpen] = useState(false)
+    const [error, setError] = useState('')
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
@@ -15,91 +24,149 @@ const SignUp = () => {
     const [password, setPassword] = useState('')
     const [confPass, setConfPass] = useState('')
 
+    const handleError = (error) => {
+        setError(error)
+        setOpen(true);
+      };
+    
+      const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        
+        setError('')
+        setOpen(false);
+      };
+
     const handleSignUp = async(e) => {
         if(password !== confPass){
-            alert('Passwords do not match.')
+            handleError("Passwords do not match."); 
             return; 
         }
 
-        await createUser(email, password, firstName, lastName)
-        navigate('/home')
+        try{
+            await createUser(email, password, firstName, lastName)
+            navigate('/home')
+        }
+        catch(error){
+            if(error.code === 'auth/invalid-email')
+            handleError("Invalid email address."); 
+        }
     }
 
     return(
         <div className="sign-in">
 
             <div className="card">
-                
-                <div>
-                    <h1>Sign Up</h1>
-                    <TextField
-                        required
-                        value={firstName}
-                        id="firstName"
-                        label="First Name"
-                        type="text"
-                        onChange={(e) => setFirstName(e.target.value)}
-                    />
+                <Card sx={{ minWidth: 450, minHeight: 575 }}>
+                    <CardContent>
+                        <h1>Sign Up</h1>
 
-                    <TextField
-                        required
-                        value={lastName}
-                        id="lastName"
-                        label="Last Name"
-                        type="text"
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
-                </div>
+                        <div className='form-field'>
+                        <Box
+                            component="form"
+                            sx={{
+                                '& > :not(style)': { m: 1, width: '25ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <TextField
+                                required
+                                value={firstName}
+                                id="firstName"
+                                label="First Name"
+                                type="text" 
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
 
-                <div>
-                    <TextField
-                        required
-                        value={bday}
-                        id="date"
-                        label="Birthday"
-                        type="date"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        onChange={(e) => setBday(e.target.value)}
-                    />
-                </div>
+                            <TextField
+                                required
+                                value={lastName}
+                                id="lastName"
+                                label="Last Name"
+                                type="text"
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
 
-                <div>
-                    <TextField
-                        required
-                        value={email}
-                        id="email"
-                        label="Email"
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
+                            </Box>
+                        </div>
 
-                <div>
-                    <TextField
-                        required
-                        value={password}
-                        id="password"
-                        label="Password"
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                        <div className='form-field'>
+                            <TextField
+                                required
+                                value={bday}
+                                id="date"
+                                label="Birthday"
+                                type="date"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                onChange={(e) => setBday(e.target.value)}
+                            />
+                        </div>
 
-                    <TextField
-                        required
-                        value={confPass}
-                        id="confPass"
-                        label="Confirm Password"
-                        type="password"
-                        onChange={(e) => setConfPass(e.target.value)}
-                    />
-                </div>
+                        <div className='form-field'>
+                            <TextField
+                                required
+                                value={email}
+                                id="email"
+                                label="Email"
+                                type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
 
-                <Button onClick={handleSignUp} variant="contained">Sign Up</Button>
+                        <div className='form-field'>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& > :not(style)': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                            <TextField
+                                required
+                                value={password}
+                                id="password"
+                                label="Password"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
 
-                <p>Already have an account? Sign in <a href='/sign-in'>here</a>!</p>
+                            <TextField
+                                required
+                                value={confPass}
+                                id="confPass"
+                                label="Confirm Password"
+                                type="password"
+                                onChange={(e) => setConfPass(e.target.value)}
+                            />
+                            </Box>
+                        </div>
+
+                        <p>Already have an account? Sign in <a href='/sign-in'>here</a>!</p>
+
+                    </CardContent>
+                    <CardActions>
+                        <Button onClick={handleSignUp} variant="contained">Sign Up</Button>
+                    </CardActions>
+                </Card>
             </div>
+
+            <Snackbar
+                open={open}
+                autoHideDuration={5000}
+                onClose={handleClose}
+            >
+                <Alert 
+                    onClose={handleClose}
+                    severity="error"
+                >
+                    {error}
+                </Alert>
+            </Snackbar> 
         </div>
     )
 }
